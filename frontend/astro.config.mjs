9b8b75e535c5a51565654429435dd7ceb2f93203
futuroptimist <80668851+futuroptimist@users.noreply.github.com>
2022-08-19 20:47:07 -0700

    Use netlify edge functions

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify/edge-functions';
 
 export default defineConfig({
   output: 'server',
