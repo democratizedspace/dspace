af5ae6a7125585c9766274cc0d9e5c7f893bcb5a
futuroptimist <80668851+futuroptimist@users.noreply.github.com>
2022-08-20 18:55:48 -0700

    Use cookies for persisting data between sessions.
    Now if someone accepts the 3D printer they don't have to accept again.
    
    Added a placeholder long-running task

---
+++ frontend/src/utils.js
@@ -0,0 +1,28 @@
+export const parseCookie = (cookie) => {
+    try {
+        return cookie.split(/[;] */).reduce(function(result, pairStr) {
+            const arr = pairStr.split('=');
+            if (arr.length === 2) {
+                result[arr[0]] = arr[1];
+            }
+            return result;
+        }, {});
+    } catch (e) {
+        return {};
+    }
+}
+
+export const getCookieValue = (cookie, key) => {
+    const c = parseCookie(cookie);
+    if (c) {
+        return c[key];
+    }
+    return null;
+}
+
+export const parseBool = (value) => {
+    if (value === 'true') {
+        return true;
+    }
+    return false;
+}
