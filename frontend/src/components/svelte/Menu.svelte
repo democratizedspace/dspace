542bf2acd66556f7e2dd384fa3747d940e9249da
futuroptimist <80668851+futuroptimist@users.noreply.github.com>
2023-01-07 00:25:11 -0800

    divide menu into pinned and unpinned
    also, highlight the current page!

---
+++ frontend/src/components/svelte/Menu.svelte
@@ -0,0 +1,120 @@
+<script>
+    import menu from '../../config/menu.json';
+
+	export let pathname;
+
+	const toggleShowUnpinned = () => {
+		console.log("toggling showUnpinned");
+		showUnpinned = !showUnpinned;
+
+		// edit unpinned-toggle button text
+		const button = document.getElementById('unpinned-toggle');
+		button.innerText = showUnpinned ? LABEL_FEWER : LABEL_MORE;
+	}
+
+	const isActive = (item) => {
+		if (item.href === '/') {
+			return pathname === '/';
+		}
+
+		// TODO: support unpinned menu items
+		return pathname.startsWith(item.href);
+	}
+
+	const LABEL_MORE = 'More';
+	const LABEL_FEWER = 'Less';
+
+	let showUnpinned = false;
+    
+	// filter menu to only pinned == true
+	const {pinned, unpinned } = menu.reduce((acc, item) => {
+		if (item.pinned) {
+			acc.pinned.push(item);
+		} else {
+			acc.unpinned.push(item);
+		}
+		return acc;
+	}, { pinned: [], unpinned: [] });
+
+	const activeUnpinned = unpinned.filter(item => isActive(item));
+	if (activeUnpinned.length > 0) {
+		const activeItem = activeUnpinned[0];
+		unpinned.splice(unpinned.indexOf(activeItem), 1);
+		pinned.push(activeItem);
+	}
+</script>
+
+<nav>
+    {#each pinned as item}
+			{#if isActive(item)}
+				<a class="active" href={item.href}>{item.name}</a>
+			{:else}
+				<a href={item.href}>{item.name}</a>
+			{/if}
+				
+    {/each}
+
+	{#if showUnpinned}
+		{#each unpinned as item}
+			<a href={item.href}>{item.name}</a>
+		{/each}
+	{/if}
+	
+	<button id="unpinned-toggle" on:click={toggleShowUnpinned}>{LABEL_MORE}</button>
+</nav>
+
+<style>
+	nav {
+		text-align: center;
+		display: inline-flex;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+
+	nav a {
+		opacity: 0.8;
+		background-color: #007006;
+		border-radius: 0.4rem;
+		color: white;
+		text-decoration: none;
+		flex-direction: row;
+		margin: 1px;
+		padding: 5px;
+		text-align: center;
+	}
+
+	nav a:hover {
+		opacity: 1;
+	}
+
+	nav button {
+		background-color: #007006;
+		border-radius: 0.4rem;
+		color: white;
+		text-decoration: none;
+		flex-direction: row;
+		margin: 1px;
+		padding: 5px;
+		text-align: center;
+		font-size: 0.7rem;
+		border: none;
+		opacity: 0.8;
+		font-family: system-ui,sans-serif;
+		/* make the height less */
+		height: 1.5rem;
+		/* center in nav */
+		align-self: center;
+	}
+
+	nav button:hover {
+		opacity: 1;
+		/* change cursor to pointer */
+		cursor: pointer;
+	}
+
+	.active {
+		/* background color slightly lighter than #007006 */
+		background-color: #68d46d;
+		color: black;
+	}
+</style>
