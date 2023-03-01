48a0b3f9f20aed3f221ce6ed4ce396307353f6bd
futuroptimist <80668851+futuroptimist@users.noreply.github.com>
2023-02-28 23:26:01 -0800

    create quest v2 MVP of basic branching dialogue

--- frontend/src/pages/quests/Quests.svelte
+++ frontend/src/pages/quests/Quests.svelte
@@ -1,19 +1,66 @@
 <script>
+    import Chip from '../../components/svelte/Chip.svelte';
+
     export let quests = [];
+    let questMap = quests.reduce((map, quest) => {
+        map[quest.id] = quest;
+        return map;
+    }, {});
+    let startingDialogue = {};
+    let dialogue = {};
 
     import { onMount } from 'svelte';
 
     onMount(() => {
-        console.log('mounted');
+        quests.forEach(quest => {
+            startingDialogue[quest.id] = quest.start;
+            dialogue[quest.id] = quest.dialogue.find(d => d.id === quest.start);
+        });
     });
+
+    function appendDialogue(questId, dialogueId) {
+        console.log("questId", questId);
+        console.log("dialogueId", dialogueId);
+        const quest = questMap[questId];
+        console.log("quest", quest);
+        const nextDialogue = quest.dialogue.find(d => d.id === dialogueId);
+        console.log("nextDialogue", nextDialogue);
+        dialogue[questId] = nextDialogue;
+        console.log("dialogue", dialogue);
+    }
+
+    function dismissQuest(questId) {
+        // replace quests[questId] with a new object that has dismissed: true
+        const index = quests.findIndex(q => q.id === questId);
+        quests[index] = {
+            ...quests[index],
+            dismissed: true
+        };
+    }
 </script>
 
 <div class="container">
     <h3>Up Next</h3>
 
     {#each quests as quest}
-        <div class="quest">
-            <h4>{quest.title}</h4>
-        </div>
+        {#if !quest.dismissed}
+            <div class="quest">
+                <h4>{quest.title}</h4>
+                {#if dialogue[quest.id]}
+                    <p>{dialogue[quest.id].text}</p>
+                    {#each dialogue[quest.id].options as option}
+                        {#if option.type === 'goto'}
+                            <div on:click={appendDialogue(quest.id, option.goto)}>
+                                <Chip text={option.text} />
+                            </div>
+                        {:else if option.type === 'dismiss'}
+                            <div on:click={dismissQuest(quest.id)}>
+                                <Chip text={option.text} />
+                            </div>
+                        {/if}
+                    {/each}
+                {/if}
+            </div>
+        {/if}
     {/each}
 </div>
