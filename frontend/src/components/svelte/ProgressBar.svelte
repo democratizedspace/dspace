142e99f50fa307ce2ef8e5e5fca86ce2863056e0
futuroptimist <80668851+futuroptimist@users.noreply.github.com>
2023-06-07 00:14:24 -0700

    add v2 goldfish quest
    also fix a bug where the remaining time on processes starts from totalDurationInSeconds on refresh

--- frontend/src/components/svelte/ProgressBar.svelte
+++ frontend/src/components/svelte/ProgressBar.svelte
@@ -1,78 +1,71 @@
 <script>  
   import { createEventDispatcher } from 'svelte';  
   import { tweened } from 'svelte/motion';  
-  import { prettyPrintDuration } from '../../utils.js'; // Adjust path as needed  
+  import { prettyPrintDuration } from '../../utils.js';  
 
   const dispatch = createEventDispatcher();  
-
   export let startValue;  
   export let totalDurationInSeconds;  
 
-  const value = tweened(startValue * 100, {duration: 0});  
-  let percentage = startValue * 100;  
-  let remainingTime = prettyPrintDuration(totalDurationInSeconds); // Initialize remainingTime  
-  let stopTween; // To store the stop function for the tween  
-  let startTimeTimestamp; // Changed from endTimeTimestamp  
+  let percentageValue = tweened(startValue * 100, {duration: 0});  
+  let stopTween;  
+  let startTime;  
   let intervalId;  
-  let mounted = false;  
+  let remainingTime = calculateRemainingTime();  
+
+  function calculateRemainingTime(elapsed = 0) {
+    return prettyPrintDuration(totalDurationInSeconds - startValue * totalDurationInSeconds - elapsed);
+  }
+
+  function stopTweenAndClearInterval() {
+    if (stopTween) stopTween();  
+    clearInterval(intervalId);
+  }
+
+  function startUpdateInterval() {
+    updateInterval();
+    intervalId = setInterval(updateInterval, 100);  
+  }
+
+  function updateInterval() {
+    const elapsed = (Date.now() - startTime) / 1000; 
+    remainingTime = calculateRemainingTime(elapsed); 
+  }
 
   export function startProgressBar() {  
-    // Stop any previous tween if exists  
-    if (stopTween) {  
-      stopTween();  
-    }  
-    // Start the tween, and store the stop function  
-    stopTween = value.subscribe(v => {  
-      percentage = v;  
+    stopTweenAndClearInterval();  
+
+    stopTween = percentageValue.subscribe(v => {  
       if (v >= 100) {  
         dispatch('processcomplete');  
         clearInterval(intervalId);  
       }  
     });  
-    value.set(100, {  
-      duration: totalDurationInSeconds * 1000,  
-      easing: x => x // linear easing  
-    });  
 
-    // Record the starting time
-    startTimeTimestamp = Date.now();
-
-    // Start the interval immediately, instead of after 1 second
-    updateInterval();
-
-    intervalId = setInterval(updateInterval, 100);  
+    percentageValue.set(100, { duration: totalDurationInSeconds * 1000, easing: x => x });  
+    startTime = Date.now();
+    startUpdateInterval(); 
   }  
 
   export function resetProgressBar() {  
-    // Stop any previous tween if exists  
-    if (stopTween) {  
-      stopTween();  
-    }  
-    value.set(0, { duration: 0 });  
-    clearInterval(intervalId);  
-
-    // Reset remainingTime to the total duration when the progress bar is reset
-    remainingTime = prettyPrintDuration(totalDurationInSeconds);  
+    stopTweenAndClearInterval();  
+    percentageValue.set(0, { duration: 0 });  
+    remainingTime = calculateRemainingTime(0);  
   }  
 
-  // Separate function for interval updates
-  function updateInterval() {
-    const elapsed = (Date.now() - startTimeTimestamp) / 1000; // Adjust elapsed calculation  
-    remainingTime = prettyPrintDuration(totalDurationInSeconds - elapsed); // Update remainingTime instead of duration  
-
-    if (!mounted) {  
-      mounted = true;  
-    }  
+  $: {
+    percentageValue.set(startValue * 100, {duration: 0}); // reactive statement for startValue changes
+    remainingTime = calculateRemainingTime(0); // reactive statement for totalDurationInSeconds changes
   }
 </script>
 
 <div class="progress-bar-container">  
   <p>
-    {$value.toFixed(2)}%
-    {#if $value < 100} - {remainingTime}{/if}
+    {$percentageValue.toFixed(2)}%
+    {#if $percentageValue < 100} - {remainingTime}{/if}
   </p>  
   <div class="progress-bar">
-    <div class="progress-bar-inner" style="width: {$value}%;"></div>
+    <div class="progress-bar-inner" style="width: {$percentageValue}%;"></div>
   </div>
 </div>
 
