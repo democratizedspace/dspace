25bd6a29f1863f9403a1a5022f1d5d97f852752c
futuroptimist <daniel@futuroptimist.com>
2023-06-21 23:15:56 -0700

    add slight delay to CompactItemList links
    Add 0.1s delay to item links in CompactItemList. This prevents accidental double clicks, which are a jarring user experience.
    
    Fixes #42

--- frontend/src/components/svelte/CompactItemList.svelte
+++ frontend/src/components/svelte/CompactItemList.svelte
@@ -5,6 +5,7 @@
     import { getItemCounts } from '../../utils/gameState.js';
     import { prettyPrintNumber } from '../../utils.js';
     import Chip from './Chip.svelte';
+    import DelayedRender from './DelayedRender.svelte';
 
     export let itemList = [], increase = false, disabled = false, noRed = false, inverted = false;
 
@@ -34,29 +35,39 @@
 </script>
 
 {#if isMounted}
-    <Chip inverted={!inverted} disabled={disabled} text="">
-        <div class="vertical">
-            {#each fullItemList as item (item.id)}
-                <div class="horizontal">
-                    <a href={`/inventory/item/${item.id}`}><img src={item.image} class="icon" alt={item.name} /></a>
-                    <p class:disabled={disabled || ($itemCounts[item.id] < item.count)} class:inverted="{inverted}">
-                        {prettyPrintNumber($itemCounts[item.id])} 
-                        {#if item.count !== null}
-                            <span class="{increase ? 'blue' : (noRed ? '' : 'red')}">{increase ? '+' : '/'}{prettyPrintNumber(item.count)}</span>
-                        {/if} 
-                        x {item.name}
-                    </p>
-                </div>
-            {/each}
-        </div>
-    </Chip>
+    <div class="Container">
+        <Chip inverted={!inverted} disabled={disabled} text="">
+            <div class="vertical">
+                {#each fullItemList as item (item.id)}
+                    <div class="horizontal">
+                        <DelayedRender delaySeconds={0.1}>
+                            <span slot="content">
+                                <a href={`/inventory/item/${item.id}`}>
+                                    <img src={item.image} class="icon" alt={item.name} />
+                                </a>
+                            </span>
+
+                            <span slot="fallback">
+                                <img src={item.image} class="icon" alt={item.name} />
+                            </span>
+                        </DelayedRender>
+                        <p class:disabled={disabled || ($itemCounts[item.id] < item.count)} class:inverted="{inverted}">
+                            {prettyPrintNumber($itemCounts[item.id])} 
+                            {#if item.count !== null}
+                                <span class ="{increase ? 'blue' : (noRed ? '' : 'red')}">{increase ? '+' : '/'}{prettyPrintNumber(item.count)}</span>
+                            {/if} 
+                            x {item.name}
+                        </p>
+                    </div>
+                {/each}
+            </div>
+        </Chip>
+    </div>
 {/if}
 
 <style>
     .vertical, .horizontal {
         display: flex;
-        justify-content: center;
-        align-items: center;
     }
     
     .vertical { flex-direction: column; }
@@ -66,16 +77,17 @@
         width: 30px;
         height: 30px;
         object-fit: cover;
-        margin: 5px;
+        margin: 5px;    
         border-radius: 20px;
     }
     
     p {
         margin: 0px;
+        margin-top: 10px;
     }
 
     .blue {
-        color: rgb(78, 175, 255);
+        color: rgb(0, 118, 215);
     }
 
     .red {
---
+++ frontend/src/components/svelte/DelayedRender.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onMount } from 'svelte';
+
+    export let delaySeconds = 5;
+    let mounted = false;
+
+    onMount(() => {
+        setTimeout(() => {
+            mounted = true;
+        }, delaySeconds * 1000);
+    });
+</script>
+
+<div>
+    {#if mounted}
+        <slot name="content" />
+    {:else}
+        <slot name="fallback" />
+    {/if}
+</div>
+
+<style>
+</style>
