184d67ccd8421493bdd9f88fef2a68340a55bbfa
futuroptimist <80668851+futuroptimist@users.noreply.github.com>
2023-07-23 22:10:07 -0700

    add dCarbon tax
    also, add some unit tests to make sure dCarbon count is correctly converted into the correct tax amount

--- frontend/__tests__/gameState/inventory.test.js
+++ frontend/__tests__/gameState/inventory.test.js
@@ -5,7 +5,6 @@ jest.mock("../../src/utils/gameState/common.js", () => {
   return {
     loadGameState: jest.fn(),
     saveGameState: jest.fn(),
-    // Add other exports here if needed
   };
 });
 
@@ -17,13 +16,14 @@ describe("gameState - inventory", () => {
     mockGameState = {
       inventory: {
         "1": 10, // Arbitrary initial inventory for item with id "1"
-        "24": 50 // Arbitrary initial dUSD inventory
+        "24": 50, // Arbitrary initial dUSD inventory
+        "20": 0   // Initializing dCarbon inventory
       }
     };
 
     // Set up what the mock functions should do
     loadGameState.mockImplementation(() => mockGameState);
-    saveGameState.mockImplementation((newState) => mockGameState = newState);
+    saveGameState.mockImplementation((newState) => { mockGameState = newState; });
 
     // Clear the mock implementations' histories
     loadGameState.mockClear();
@@ -40,12 +40,79 @@ describe("gameState - inventory", () => {
     sellItems(itemsToSell);
 
     // Assert
-    expect(loadGameState).toHaveBeenCalled();
+    expect(loadGameState).toHaveBeenCalledTimes(1);
     expect(saveGameState).toHaveBeenCalledTimes(1);
     
     const expectedInventory = {
       "1": 10, // Quantity of item "1" should not have changed
-      "24": 50 // Quantity of dUSD should not have changed
+      "24": 50, // Quantity of dUSD should not have changed
+      "20": 0 // dCarbon inventory should not have changed
+    };
+
+    expect(mockGameState.inventory).toEqual(expectedInventory);
+  });
+
+  test("No tax should be applied when there's no dCarbon in the inventory", () => {
+    // Set up for this specific test case
+    mockGameState.inventory["20"] = 0;  // No dCarbon in the inventory
+
+    const itemsToSell = [
+      { id: "1", quantity: 5, price: 2 }
+    ];
+
+    sellItems(itemsToSell);
+
+    expect(loadGameState).toHaveBeenCalledTimes(1);
+    expect(saveGameState).toHaveBeenCalledTimes(1);
+
+    const expectedInventory = {
+      "1": 5, // Quantity of item "1" should have decreased by 5
+      "24": 60, // Quantity of dUSD should have increased by 10 (5 items * price 2)
+      "20": 0 // dCarbon inventory should not have changed
+    };
+
+    expect(mockGameState.inventory).toEqual(expectedInventory);
+  });
+
+  test("Tax should be applied correctly based on the amount of dCarbon", () => {
+    // Adding dCarbon to the inventory
+    mockGameState.inventory["20"] = 2000;
+
+    const itemsToSell = [
+      { id: "1", quantity: 5, price: 2 }
+    ];
+
+    sellItems(itemsToSell);
+
+    expect(loadGameState).toHaveBeenCalledTimes(1);
+    expect(saveGameState).toHaveBeenCalledTimes(1);
+
+    const expectedInventory = {
+      "1": 5, // Quantity of item "1" should have decreased by 5
+      "20": 2000, // dCarbon should remain the same
+      "24": 58 // Quantity of dUSD should have increased by 8 (5 items * price 2 * 0.8 due to 20% tax)
+    };
+
+    expect(mockGameState.inventory).toEqual(expectedInventory);
+  });
+
+  test("No more tax should be applied once dCarbon hits the max tax rate", () => {
+    // Adding dCarbon to the inventory
+    mockGameState.inventory["20"] = 10000;
+
+    const itemsToSell = [
+      { id: "1", quantity: 5, price: 2 }
+    ];
+
+    sellItems(itemsToSell);
+
+    expect(loadGameState).toHaveBeenCalledTimes(1);
+    expect(saveGameState).toHaveBeenCalledTimes(1);
+
+    const expectedInventory = {
+      "1": 5, // Quantity of item "1" should have decreased by 5
+      "20": 10000, // dCarbon should remain the same
+      "24": 51 // Quantity of dUSD should have increased by 1 (5 items * price 2 * 0.1 due to 90% tax)
     };
 
     expect(mockGameState.inventory).toEqual(expectedInventory);
--- frontend/src/utils/gameState/inventory.js
+++ frontend/src/utils/gameState/inventory.js
@@ -59,26 +59,57 @@ export const addItems = (items) => {
     saveGameState(gameState);
   };
   
+  /**
+   * Modifies the game state to reflect selling items from the inventory. The
+   * function retrieves the game state, processes the provided items, and then
+   * saves the modified game state.
+   *
+   * For every 1000 dCarbon in your inventory, a 10% tax is applied to the selling
+   * price. The maximum tax applied is 90%, i.e., once you have 9000 or more dCarbon.
+   * 
+   * @param {Array} items - Array of items to be sold. Each item is an object
+   *                        with `price` and `quantity` properties.
+   */
   export const sellItems = (items) => {
+    // Load the current game state
     const gameState = loadGameState();
   
+    // The id of the dUSD item
+    const currencyId = "24";
+
+    // The id of the dCarbon item
+    const dCarbonId = "20";
+  
+    // Loop through all the items to be sold
     items.forEach(item => {
       const { price, quantity } = item;
-      const currencyId = "24"; // Assuming the currency ID is always "24"
   
       // Ignore items with non-positive quantity
       if (quantity <= 0) {
         return;
       }
   
+      // Only proceed if there is enough of the item in the inventory
       if (gameState.inventory[item.id] && gameState.inventory[item.id] >= quantity) {
+        // Decrease the item quantity in the inventory
         gameState.inventory[item.id] -= quantity;
-        gameState.inventory[currencyId] = (gameState.inventory[currencyId] || 0) + price * quantity;
+  
+        // Calculate the tax based on the amount of dCarbon in the inventory
+        const dCarbonCount = gameState.inventory[dCarbonId] || 0;
+        const tax = Math.min(Math.floor(dCarbonCount / 1000) * 0.10, 0.90);
+  
+        // Calculate the total price considering the tax
+        const taxedPrice = price * (1 - tax);
+  
+        // Increase the dUSD count in the inventory
+        gameState.inventory[currencyId] = (gameState.inventory[currencyId] || 0) + taxedPrice * quantity;
       }
     });
   
+    // Save the updated game state
     saveGameState(gameState);
-  };  
+  };
+
     
   export const hasItems = (itemList) => {
     const gameState = loadGameState();
