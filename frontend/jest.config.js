271a2a2bc5eb64c758d2c5f7adf7b5a3acb7707f
futuroptimist <80668851+futuroptimist@users.noreply.github.com>
2023-08-02 13:37:41 -0700

    include svelte files in coverage estimate
    `npm run coverage`

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -10,4 +10,8 @@ module.exports = {
   transformIgnorePatterns: ['node_modules'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   roots: ['./src', './__tests__'],
+  collectCoverageFrom: [
+    'src/**/*.{js,svelte}',
+    '!**/node_modules/**',
+  ],
 };
