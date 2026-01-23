43c7b0f6d189ca78b8f1c5ebaa119b329e26005e
futuroptimist <80668851+futuroptimist@users.noreply.github.com>
2026-01-23 15:22:26 -0800

    Remove redundant quest banner sizing

--- frontend/src/pages/quests/svelte/QuestChat.svelte
+++ frontend/src/pages/quests/svelte/QuestChat.svelte
@@ -241,8 +241,6 @@
         width: 100%;
         height: 100%;
         object-fit: cover;
-        max-width: 100%;
-        max-height: 100%;
         display: block;
         margin: 0;
         border-radius: 0;
